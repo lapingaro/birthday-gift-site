8c383dab6ae031ab44d67bf425110398c4707653
lapingaro <chrisbehint@gmail.com>
2023-12-13 12:54:36 +0200

    commit initial file

---
+++ index.js
@@ -0,0 +1,7 @@
+
+ userNameEl=document.getElementById("userName-el");
+ passwordEl=document.getElementById("password-el");
+function signUp(){
+
+    console.log("The user name is : " + userNameEl.textContent + " and the password is: " + passwordEl.textContent) 
+}
